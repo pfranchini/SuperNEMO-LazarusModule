6d57defce4db6cace5ae752a94be2702f958f3e4
Paolo Franchini <pfranchini@gmail.com>
2020-06-05 22:09:42 +0100

    Minor change in efficiency plotting

--- scripts/plot_efficiency.py
+++ scripts/plot_efficiency.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('efficiency.txt') as f:
+with open('efficiency_BETWEEN.txt') as f:
     lines = f.readlines()
     dead_cells = [line.split()[0] for line in lines]
     efficiency = [line.split()[1] for line in lines]
@@ -26,7 +26,7 @@ ax1.set_ylabel('[%]')
 ax1.plot(dead_cells,efficiency, color='green', label='Efficiency', marker='o')
 ax1.plot(dead_cells,zero, color='red', label='Zero reconstructed tracks', marker='o')
 ax1.plot(dead_cells,single, color='yellow', label='Single track', marker='o')
-ax1.plot(dead_cells,more, color='blue', label='More than one reconstructed track', marker='o')
+ax1.plot(dead_cells,more, color='blue', label='More than two reconstructed tracks', marker='o')
 
 leg = ax1.legend(loc='upper left', fontsize='small')
 
